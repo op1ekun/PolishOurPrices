08051a76a4fcf797427dfdfe919a75b039ec9fe4
op1ekun <op1ekun@borntoco.de>
2024-09-04 21:05:21 +0200

    adds name mapping for quick lookup

--- get_price_overview.js
+++ get_price_overview.js
@@ -5,21 +5,39 @@ const { applist: { apps } } = require('./applist.json');
 const requestLimit = 1000;
 let appIdsIndex = 0;
 
-const filteredAppIds = apps
-    .filter(app => app.name && !app.name.match(/(^test)|(\s+test\s+)/))
-    .reduce((arr, { appid }) => {
-        if (!arr[appIdsIndex]) {
-            arr[appIdsIndex] = [];
+let filteredAppIds = [];
+let appNameById = {};
+for (let i = 0; i < apps.length; i++) {
+    const { appid, name } = apps[i];
+    if (name && !name.match(/(^test)|(\s+test\s+)/)) {
+        if (!filteredAppIds[appIdsIndex]) {
+            filteredAppIds[appIdsIndex] = [];
         }
-      
-        if (arr[appIdsIndex].length === requestLimit) {
+        
+        if (filteredAppIds[appIdsIndex].length === requestLimit) {
             appIdsIndex++;
-            arr[appIdsIndex] = [];
+            filteredAppIds[appIdsIndex] = [];
         }
-        arr[appIdsIndex].push(appid);
+        filteredAppIds[appIdsIndex].push(appid);
+        appNameById[appid] = name;
+    }
+}
+
+// const filteredAppIds = apps
+//     .filter(app => app.name && !app.name.match(/(^test)|(\s+test\s+)/))
+//     .reduce((arr, { appid }) => {
+//         if (!arr[appIdsIndex]) {
+//             arr[appIdsIndex] = [];
+//         }
       
-        return arr;
-    }, []);
+//         if (arr[appIdsIndex].length === requestLimit) {
+//             appIdsIndex++;
+//             arr[appIdsIndex] = [];
+//         }
+//         arr[appIdsIndex].push(appid);
+      
+//         return arr;
+//     }, []);
 
 const currencyToCountry = {
     'PLN': 'pl',
@@ -29,41 +47,38 @@ const currencyToCountry = {
     'USD': 'en'
 };
 
-const getPriceOverview = (country, currency) => {
-    return [ filteredAppIds[0], filteredAppIds[1] ]
-        .reduce((chain, appIds) => 
-            chain.then((partial) =>
-                new Promise((resolve) => {
-                    https.get(`https://store.steampowered.com/api/appdetails?appids=${appIds.join(',')}&filters=price_overview&cc=${country}`, res => {
-                        let data = [];
-                        console.log('retrieving', currency);
-                        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-                        console.log('Status Code:', res.statusCode);
-                        console.log('Date in Response header:', headerDate);
+const getPriceOverview = (country, currency) => [ filteredAppIds[0] ].reduce((chain, appIds) => 
+    chain.then((partial) =>
+        new Promise((resolve) => {
+            https.get(`https://store.steampowered.com/api/appdetails?appids=${appIds.join(',')}&filters=price_overview&cc=${country}`, res => {
+                let data = [];
+                console.log('retrieving', currency);
+                const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+                console.log('Status Code:', res.statusCode);
+                console.log('Date in Response header:', headerDate);
 
-                        res.on('data', chunk => {
-                            data.push(chunk);
-                        });
+                res.on('data', chunk => {
+                    data.push(chunk);
+                });
 
-                        res.on('end', () => {
-                            console.log('Response ended: ');
-                            const appsFr = JSON.parse(Buffer.concat(data).toString());    
-                            resolve({
-                                ...partial,
-                                ...appsFr
-                            });
-                        });
+                res.on('end', () => {
+                    console.log('Response ended: ');
+                    const appsFr = JSON.parse(Buffer.concat(data).toString());    
+                    resolve({
+                        ...partial,
+                        ...appsFr
                     });
-            })
-    ), Promise.resolve({}));
-};
+                });
+            });
+    })
+), Promise.resolve({}));
+
 
 Object.keys(currencyToCountry).reduce((chain, currency) => {
     const country = currencyToCountry[currency];
 
     return chain.then((overview) =>
         getPriceOverview(country, currency).then((result) => {
-            console.log('currency', currency);
             Object.keys(result).forEach((appId) => {
                 if (
                     result[appId] &&
@@ -72,20 +87,20 @@ Object.keys(currencyToCountry).reduce((chain, currency) => {
                 ) {
                     if (!overview[appId]) {
                         overview[appId] = {
+                            name: appNameById[appId],
                             priceOverview: {},
                         };
                     }
                 
                     overview[appId].priceOverview = {
+                        ...overview[appId].priceOverview,
                         [currency]: result[appId].data.price_overview,
                     };
                 }
             });
 
-            console.log('overview', overview);
             return overview;
         }));
-}, Promise.resolve({}))
-    .then((overview) => {
-        fs.writeFileSync(`./price_overview.json`, JSON.stringify(overview));
-    })
+}, Promise.resolve({})).then((overview) => {
+    fs.writeFileSync(`./price_overview.json`, JSON.stringify(overview));
+})
