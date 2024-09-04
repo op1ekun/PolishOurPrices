e3cfc4acd392119fda5be27bc048f4e3e1a5f42a
op1ekun <op1ekun@borntoco.de>
2024-09-04 18:47:40 +0200

    get one currency after another

--- get_price_overview.js
+++ get_price_overview.js
@@ -29,10 +29,46 @@ const currencyToCountry = {
     'USD': 'en'
 };
 
-Object.keys(currencyToCountry).forEach((currency) => {
+Object.keys(currencyToCountry).forEach(async (currency) => {
     const country = currencyToCountry[currency];
 
-    [ filteredAppIds[0] ]
+    await getPriceOverview(country, currency);
+
+    // [ filteredAppIds[0] ]
+    //     .reduce((chain, appIds) => {
+    //         return chain.then((partial) => {
+    //             return new Promise((resolve) => {
+    //                 https.get(`https://store.steampowered.com/api/appdetails?appids=${appIds.join(',')}&filters=price_overview&cc=${country}`, res => {
+    //                     let data = [];
+    //                     console.log(currency);
+    //                     const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    //                     console.log('Status Code:', res.statusCode);
+    //                     console.log('Date in Response header:', headerDate);
+
+    //                     res.on('data', chunk => {
+    //                         data.push(chunk);
+    //                     });
+
+    //                     res.on('end', () => {
+    //                         console.log('Response ended: ');
+    //                         const appsFr = JSON.parse(Buffer.concat(data).toString());    
+    //                         resolve({
+    //                             ...partial,
+    //                             ...appsFr
+    //                         });
+    //                     });
+    //                 });
+    //         });
+    //     });
+    // }, Promise.resolve({}))
+    // .then((result) => {
+    //     console.log("result", result);
+    //     fs.writeFileSync(`./price_overview_${currency}.json`, JSON.stringify(result));
+    // });
+});
+
+const getPriceOverview = (country, currency) => {
+    return [ filteredAppIds[0], filteredAppIds[1] ]
         .reduce((chain, appIds) => {
             return chain.then((partial) => {
                 return new Promise((resolve) => {
@@ -60,7 +96,8 @@ Object.keys(currencyToCountry).forEach((currency) => {
         });
     }, Promise.resolve({}))
     .then((result) => {
-        console.log("result", result);
+        console.log('result', result);
         fs.writeFileSync(`./price_overview_${currency}.json`, JSON.stringify(result));
     });
-});
+
+}
