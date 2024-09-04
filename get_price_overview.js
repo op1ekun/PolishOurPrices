fb95098647ea0f2948a37a73ddfe089538c2a6f1
op1ekun <op1ekun@borntoco.de>
2024-09-04 20:30:17 +0200

    builds all currencies overview

--- get_price_overview.js
+++ get_price_overview.js
@@ -2,7 +2,7 @@ const https = require('https');
 const fs = require('fs');
 const { applist: { apps } } = require('./applist.json');
 
-const requestLimit = 500;
+const requestLimit = 1000;
 let appIdsIndex = 0;
 
 const filteredAppIds = apps
@@ -31,9 +31,9 @@ const currencyToCountry = {
 
 const getPriceOverview = (country, currency) => {
     return [ filteredAppIds[0], filteredAppIds[1] ]
-        .reduce((chain, appIds) => {
-            return chain.then((partial) => {
-                return new Promise((resolve) => {
+        .reduce((chain, appIds) => 
+            chain.then((partial) =>
+                new Promise((resolve) => {
                     https.get(`https://store.steampowered.com/api/appdetails?appids=${appIds.join(',')}&filters=price_overview&cc=${country}`, res => {
                         let data = [];
                         console.log(currency);
@@ -54,53 +54,34 @@ const getPriceOverview = (country, currency) => {
                             });
                         });
                     });
+            })
+    ), Promise.resolve({}));
+};
+
+Object.keys(currencyToCountry).reduce((chain, currency) => {
+    const country = currencyToCountry[currency];
+
+    return chain.then((overview) =>
+        getPriceOverview(currency, country).then((result) => {
+            Object.keys(result).forEach((appId) => {
+                if (
+                    result[appId] &&
+                    result[appId].success &&
+                    !Array.isArray(result[appId].data)
+                ) {
+                    if (!overview[appId]) {
+                        overview[appId] = {
+                            priceOverview: {},
+                        };
+                    }
+                
+                    overview[appId].priceOverview = {
+                        [currency]: result[appId].data.price_overview,
+                    };
+                }
             });
-        });
-    }, Promise.resolve({}))
-    .then((result) => {
-        // console.log('result', result);
-        fs.writeFileSync(`./price_overview_${currency}.json`, JSON.stringify(result));
-    });
-}
-
-// Object.keys(currencyToCountry).forEach(async (currency) => {
-//     const country = currencyToCountry[currency];
-
-    getPriceOverview('pl', 'PLN')
-        .then(() => getPriceOverview('de', 'EUR'))
-        .then(() => getPriceOverview('gb', 'GBP'))
-        .then(() => getPriceOverview('no', 'NOK'))
-        .then(() => getPriceOverview('en', 'USD'));
-
-    // [ filteredAppIds[0] ]
-    //     .reduce((chain, appIds) => {
-    //         return chain.then((partial) => {
-    //             return new Promise((resolve) => {
-    //                 https.get(`https://store.steampowered.com/api/appdetails?appids=${appIds.join(',')}&filters=price_overview&cc=${country}`, res => {
-    //                     let data = [];
-    //                     console.log(currency);
-    //                     const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-    //                     console.log('Status Code:', res.statusCode);
-    //                     console.log('Date in Response header:', headerDate);
-
-    //                     res.on('data', chunk => {
-    //                         data.push(chunk);
-    //                     });
-
-    //                     res.on('end', () => {
-    //                         console.log('Response ended: ');
-    //                         const appsFr = JSON.parse(Buffer.concat(data).toString());    
-    //                         resolve({
-    //                             ...partial,
-    //                             ...appsFr
-    //                         });
-    //                     });
-    //                 });
-    //         });
-    //     });
-    // }, Promise.resolve({}))
-    // .then((result) => {
-    //     console.log("result", result);
-    //     fs.writeFileSync(`./price_overview_${currency}.json`, JSON.stringify(result));
-    // });
-// });
+        }));
+}, Promise.resolve({}))
+    .then((overview) => {
+        fs.writeFileSync(`./price_overview.json`, JSON.stringify(overview));
+    })
