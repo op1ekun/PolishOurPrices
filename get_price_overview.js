8a86634be68588d15cc50b0e0829edc4b2ca0261
op1ekun <op1ekun@borntoco.de>
2024-09-05 11:36:25 +0200

    moves all the requests into node.js script

--- get_price_overview.js
+++ get_price_overview.js
@@ -1,43 +1,8 @@
 const https = require('https');
 const fs = require('fs');
-const { applist: { apps } } = require('./applist.json');
 
 const requestLimit = 1000;
 let appIdsIndex = 0;
-
-let filteredAppIds = [];
-let appNameById = {};
-for (let i = 0; i < apps.length; i++) {
-    const { appid, name } = apps[i];
-    if (name && !name.match(/(^test)|(\s+test\s+)/)) {
-        if (!filteredAppIds[appIdsIndex]) {
-            filteredAppIds[appIdsIndex] = [];
-        }
-        
-        if (filteredAppIds[appIdsIndex].length === requestLimit) {
-            appIdsIndex++;
-            filteredAppIds[appIdsIndex] = [];
-        }
-        filteredAppIds[appIdsIndex].push(appid);
-        appNameById[appid] = name;
-    }
-}
-
-// const filteredAppIds = apps
-//     .filter(app => app.name && !app.name.match(/(^test)|(\s+test\s+)/))
-//     .reduce((arr, { appid }) => {
-//         if (!arr[appIdsIndex]) {
-//             arr[appIdsIndex] = [];
-//         }
-      
-//         if (arr[appIdsIndex].length === requestLimit) {
-//             appIdsIndex++;
-//             arr[appIdsIndex] = [];
-//         }
-//         arr[appIdsIndex].push(appid);
-      
-//         return arr;
-//     }, []);
 
 const currencyToCountry = {
     'PLN': 'pl',
@@ -47,12 +12,14 @@ const currencyToCountry = {
     'USD': 'en'
 };
 
-const getPriceOverview = (country, currency) => [ filteredAppIds[0] ].reduce((chain, appIds) => 
-    chain.then((partial) =>
+const getPriceOverview = (country, currency, filteredAppIds) => [ filteredAppIds[0] ].reduce((chain, appIds, index) => {
+    const requestCount = filteredAppIds.length;
+
+    return chain.then((partial) =>
         new Promise((resolve) => {
             https.get(`https://store.steampowered.com/api/appdetails?appids=${appIds.join(',')}&filters=price_overview&cc=${country}`, res => {
                 let data = [];
-                console.log('retrieving', currency);
+                console.log(`retrieved price overview partial ${index + 1}/${requestCount} for ${currency}`);
                 const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
                 console.log('Status Code:', res.statusCode);
                 console.log('Date in Response header:', headerDate);
@@ -70,37 +37,81 @@ const getPriceOverview = (country, currency) => [ filteredAppIds[0] ].reduce((ch
                     });
                 });
             });
-    })
-), Promise.resolve({}));
+        }));
+    
+}, Promise.resolve({}));
 
+new Promise((resolve) => {
+    let filteredAppIds = [];
+    let appNameById = {};
 
-Object.keys(currencyToCountry).reduce((chain, currency) => {
-    const country = currencyToCountry[currency];
+    https.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/', res => {
+        let data = [];
+        res.on('data', chunk => {
+            data.push(chunk);
+        });
+        res.on('end', () => {
+            for (let i = 0; i < data.length; i++) {
+                const { appid, name } = data[i];
+                if (name && !name.match(/(^test)|(\s+test\s+)/)) {
+                    if (!filteredAppIds[appIdsIndex]) {
+                        filteredAppIds[appIdsIndex] = [];
+                    }
+                    
+                    if (filteredAppIds[appIdsIndex].length === requestLimit) {
+                        appIdsIndex++;
+                        filteredAppIds[appIdsIndex] = [];
+                    }
+                    filteredAppIds[appIdsIndex].push(appid);
+                    appNameById[appid] = name;
+                }
+            }
+        })
+    });
 
-    return chain.then((overview) =>
-        getPriceOverview(country, currency).then((result) => {
-            Object.keys(result).forEach((appId) => {
-                if (
-                    result[appId] &&
-                    result[appId].success &&
-                    !Array.isArray(result[appId].data)
-                ) {
-                    if (!overview[appId]) {
-                        overview[appId] = {
-                            name: appNameById[appId],
-                            priceOverview: {},
+    resolve({
+        filteredAppIds,
+        appNameById
+    });
+})
+.then(({ filteredAppIds, appNameById }) => {
+    Object.keys(currencyToCountry).reduce((chain, currency) => {
+        const country = currencyToCountry[currency];
+    
+        return chain.then((overview) =>
+            getPriceOverview(country, currency, filteredAppIds).then((result) => {
+                Object.keys(result).forEach((appId) => {
+                    if (
+                        result[appId] &&
+                        result[appId].success &&
+                        !Array.isArray(result[appId].data)
+                    ) {
+                        if (!overview[appId]) {
+                            overview[appId] = {
+                                name: appNameById[appId],
+                                priceOverview: {},
+                            };
+                        }
+                    
+                        overview[appId].priceOverview = {
+                            ...overview[appId].priceOverview,
+                            [currency]: result[appId].data.price_overview,
                         };
                     }
-                
-                    overview[appId].priceOverview = {
-                        ...overview[appId].priceOverview,
-                        [currency]: result[appId].data.price_overview,
-                    };
-                }
-            });
-
-            return overview;
-        }));
-}, Promise.resolve({})).then((overview) => {
-    fs.writeFileSync(`./price_overview.json`, JSON.stringify(overview));
+                });
+    
+                return overview;
+            }));
+    }, Promise.resolve({}))
 })
+.then((overview) => {
+    fs.writeFileSync(`./price_overview.json`, JSON.stringify(overview));
+    // test static API capabilities
+    fs.writeFileSync(`./index.json`, JSON.stringify(overview));
+})
+
+
+
+
+
+
